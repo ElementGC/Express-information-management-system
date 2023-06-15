82e2af4211d1561dc6c45769ab8027fc7fb05c92
ElementGC <b20090727@njupt.edu.cn>
2023-06-15 10:43:16 +0800

    完善快递员的快递订单查询功能

--- src/express/courier/Courier.java
+++ src/express/courier/Courier.java
@@ -23,9 +23,10 @@ public class Courier extends JFrame{
     JLabel label3;
     JLabel label4;
     JLabel label5;
-    JTextField tf1;
-    JTextField tf2;
-    JTextField tf3;
+    JLabel label6;
+    JTextField tf1; //快递位置文本框
+    JTextField tf2; //快递状态文本框
+    JTextField tf3; //快递单号文本框
 
     String loc = null;
     String state = null;
@@ -47,11 +48,12 @@ public class Courier extends JFrame{
         bt1 = new JButton("录入");
         bt2 = new JButton("查询");
         back = new JButton("⬅");
-        label1 = new JLabel("快递信息录入(在输入查询处填入单号才可录入)");
+        label1 = new JLabel("快递信息录入");
+        label6 = new JLabel("(在输入查询处填入单号才可录入)");
         label2 = new JLabel("快递当前位置：");
         label3 = new JLabel("状态：");
         label4 = new JLabel("快件信息查询");
-        label5 = new JLabel("输入查询：");
+        label5 = new JLabel("输入快递单号：");
         tf1 = new JTextField();
         tf2 = new JTextField();
         tf3 = new JTextField();
@@ -81,62 +83,32 @@ public class Courier extends JFrame{
                 state = tf2.getText();
                 enquiry = tf3.getText();
                 //判断字符串是否以1开头，若是，则是快递单号，若不是，则是
-                if (enquiry.startsWith("1")) {
-                    try {
-                        //从输入的文本框里获取输入的数据，然后做对比
-                        //'"+account+"'这里这个表示的是变量account
-                        String sql = "update expressinformation set expresslocation = '" + loc + "',expressstatus = '" + state + "' where goodnum = '"+enquiry+"'";
-                        GroupLayoutTest.statement = GroupLayoutTest.conn.createStatement();
-                        int res = GroupLayoutTest.statement.executeUpdate(sql);
-                        if (res == 1){
-                            JOptionPane.showMessageDialog(null, "录入成功！",
-                                    "通知", JOptionPane.PLAIN_MESSAGE);
-                        }else {
-                            JOptionPane.showMessageDialog(null, "录入失败！",
-                                    "通知", JOptionPane.PLAIN_MESSAGE);
-                            new Courier();
-                        }
-                        //根据不同的身份返回到不同窗口
-                        if (GroupLayoutTest.account.startsWith("p")){
-                            new Courier();
-                        }else if (GroupLayoutTest.account.startsWith("s")){
-                            new Crew();
-                        }else if (GroupLayoutTest.account.startsWith("m")){
-                            new Manager();
-                        }
-                        dispose();
-                    } catch (Exception ee) {
-                        ee.printStackTrace();
-                        System.out.println("密码输入错误！请从新输入");
+                try {
+                    //从输入的文本框里获取输入的数据，然后做对比
+                    //'"+account+"'这里这个表示的是变量account
+                    String sql = "update express_info set expressposition = '" + loc + "',expressstatus = '" + state + "' where goodid = '"+enquiry+"'";
+                    GroupLayoutTest.statement = GroupLayoutTest.conn.createStatement();
+                    int res = GroupLayoutTest.statement.executeUpdate(sql);
+                    if (res == 1){
+                        JOptionPane.showMessageDialog(null, "录入成功！",
+                                "通知", JOptionPane.PLAIN_MESSAGE);
+                    }else {
+                        JOptionPane.showMessageDialog(null, "录入失败！",
+                                "通知", JOptionPane.PLAIN_MESSAGE);
+                        new Courier();
                     }
-                }else{
-                    try {
-                        //从输入的文本框里获取输入的数据，然后做对比
-                        //'"+account+"'这里这个表示的是变量account
-                        String sql = "update expressinformation set expresslocation = '" + loc + "',expressstatus = '" + state + "' where receiver = '"+enquiry+"'";
-                        GroupLayoutTest.statement = GroupLayoutTest.conn.createStatement();
-                        int res = GroupLayoutTest.statement.executeUpdate(sql);
-                        if (res == 1){
-                            JOptionPane.showMessageDialog(null, "录入成功！",
-                                    "通知", JOptionPane.PLAIN_MESSAGE);
-                        }else {
-                            JOptionPane.showMessageDialog(null, "录入失败！",
-                                    "通知", JOptionPane.PLAIN_MESSAGE);
-                            new Courier();
-                        }
-                        //根据不同的身份返回到不同窗口
-                        if (GroupLayoutTest.account.startsWith("p")){
-                            new Courier();
-                        }else if (GroupLayoutTest.account.startsWith("s")){
-                            new Crew();
-                        }else if (GroupLayoutTest.account.startsWith("m")){
-                            new Manager();
-                        }
-                        dispose();
-                    } catch (Exception ee) {
-                        ee.printStackTrace();
-                        System.out.println("密码输入错误！请从新输入");
+                    //根据不同的身份返回到不同窗口
+                    if (GroupLayoutTest.account.startsWith("p")){
+                        new Courier();
+                    }else if (GroupLayoutTest.account.startsWith("s")){
+                        new Crew();
+                    }else if (GroupLayoutTest.account.startsWith("m")){
+                        new Manager();
                     }
+                    dispose();
+                } catch (Exception ee) {
+                    ee.printStackTrace();
+                    System.out.println("密码输入错误！请从新输入");
                 }
             }
         });
@@ -146,46 +118,23 @@ public class Courier extends JFrame{
             @Override
             public void actionPerformed(ActionEvent e) {
                 enquiry = tf3.getText();
-                if (enquiry.startsWith("1")) {
-                    try {
-                        //从输入的文本框里获取输入的数据，然后做对比
-                        //'"+account+"'这里这个表示的是变量account
-                        String sql = "select * from expressinformation where goodnum = '"+enquiry+"'";
-                        GroupLayoutTest.statement = GroupLayoutTest.conn.createStatement();
-                        ResultSet res = GroupLayoutTest.statement.executeQuery(sql);
-                        String s = null;
-                        while (res.next()){
-                            s +="编号为" + res.getString("goodnum") + "的快递";
-                            s += "由" + res.getString("pid") + "快递员派送，";
-                            s += "当前在" + res.getString("expresslocation") + "，" + res.getString("expressstatus");
-                        }
-                        s = s.substring(4);
-                        JOptionPane.showMessageDialog(null, s,
-                                "快件信息", JOptionPane.PLAIN_MESSAGE);
-                    } catch (Exception ee) {
-                        ee.printStackTrace();
-                        System.out.println("密码输入错误！请从新输入");
+                try {
+                    //从输入的文本框里获取输入的数据，然后做对比
+                    String sql = "select * from express_info where goodid = '"+enquiry+"'";
+                    GroupLayoutTest.statement = GroupLayoutTest.conn.createStatement();
+                    ResultSet res = GroupLayoutTest.statement.executeQuery(sql);
+                    String s = null;
+                    while (res.next()){
+                        s +="编号为" + res.getString("goodid") + "的快递";
+                        s += "由" + res.getString("pid") + "快递员派送，";
+                        s += "当前在" + res.getString("expressposition") + "，" + res.getString("expressstatus");
                     }
-                }else{
-                    try {
-                        //从输入的文本框里获取输入的数据，然后做对比
-                        //'"+account+"'这里这个表示的是变量account
-                        String sql = "select * from expressinformation where receiver = '"+enquiry+"'";
-                        GroupLayoutTest.statement = GroupLayoutTest.conn.createStatement();
-                        ResultSet res = GroupLayoutTest.statement.executeQuery(sql);
-                        String s = null;
-                        while (res.next()){
-                            s +="：编号为" + res.getString("goodnum") + "的快递";
-                            s += "由" + res.getString("pid") + "快递员派送，";
-                            s += "当前在" + res.getString("expresslocation") + "，" + res.getString("expressstatus");
-                        }
-                        s = s.substring(4);
-                        JOptionPane.showMessageDialog(null, s,
-                                "快件信息", JOptionPane.PLAIN_MESSAGE);
-                    } catch (Exception ee) {
-                        ee.printStackTrace();
-                        System.out.println("密码输入错误！请从新输入");
-                    }
+                    s = s.substring(4);
+                    JOptionPane.showMessageDialog(null, s,
+                            "快件信息", JOptionPane.PLAIN_MESSAGE);
+                } catch (Exception ee) {
+                    ee.printStackTrace();
+                    System.out.println("密码输入错误！请从新输入");
                 }
             }
         });
@@ -203,7 +152,7 @@ public class Courier extends JFrame{
                 addComponent(label2).addComponent(bt1).addComponent(label4).addComponent(label5)
                 .addComponent(bt2));
         hGroup.addGap(5);
-        hGroup.addGroup(layout.createParallelGroup().addComponent(tf1).addComponent(tf3));
+        hGroup.addGroup(layout.createParallelGroup().addComponent(label6).addComponent(tf1).addComponent(tf3));
         hGroup.addGap(10);
         hGroup.addGroup(layout.createParallelGroup().addComponent(label3));
         hGroup.addGroup(layout.createParallelGroup().addComponent(tf2));
@@ -216,7 +165,7 @@ public class Courier extends JFrame{
         vGroup.addGap(0);
         vGroup.addGroup(layout.createParallelGroup().addComponent(back));
         vGroup.addGap(15);
-        vGroup.addGroup(layout.createParallelGroup().addComponent(label1));
+        vGroup.addGroup(layout.createParallelGroup().addComponent(label1).addComponent(label6));
         vGroup.addGap(5);
         vGroup.addGroup(layout.createParallelGroup().addComponent(label2).addComponent(tf1).addComponent(label3).addComponent(tf2));
         vGroup.addGap(5);
